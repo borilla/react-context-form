edbc780c5031d0833138bc4b0259f36f6e5c1f3a
borilla <github@borilla.co.uk>
2019-03-27 01:28:25 +1000

    Rename some stuff
    - To try to make `index.js` a little more readable

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ import ReactDOM from 'react-dom';
 import { FormSection, FormInput, FormCheckbox, FormSubmit } from './src/form-components';
 
 function App(props) {
-	const [formValue, setFormValue] = React.useState({ name: 'John', acceptTerms: false });
+	const [state, setState] = React.useState({ name: 'John', acceptTerms: false });
 
 	function onChange({ getValue }) {
-		const formValue = getValue();
-		setFormValue(formValue);
-		console.log('Change:', formValue);
+		const newState = getValue();
+		setState(newState);
+		console.log('Change:', newState);
 	}
 
 	function onSubmit({ getValue }) {
-		const formValue = getValue();
-		setFormValue(formValue);
-		console.log('Submit:', formValue);
+		const newState = getValue();
+		setState(newState);
+		console.log('Submit:', newState);
 	}
 
 	return (
 		<FormSection onChange={onChange} onSubmit={onSubmit}>
-			<FormInput name="name" label="Name" initialValue={formValue.name} />
+			<FormInput name="name" label="Name" initialValue={state.name} />
 			<FormInput name="phone" label="Phone" />
 			<FormInput name="email" label="Email" />
 			<FormSection name="address">
@@ -27,8 +27,8 @@ function App(props) {
 				<FormInput name="line2" />
 				<FormInput name="postalCode" label="Postal Code" />
 			</FormSection>
-			<FormCheckbox name="acceptTerms" label="Accept Terms" initialValue={formValue.acceptTerms} />
-			<FormSubmit disabled={!formValue.acceptTerms}>Submit</FormSubmit>
+			<FormCheckbox name="acceptTerms" label="Accept Terms" initialValue={state.acceptTerms} />
+			<FormSubmit disabled={!state.acceptTerms}>Submit</FormSubmit>
 		</FormSection>
 	);
 }
